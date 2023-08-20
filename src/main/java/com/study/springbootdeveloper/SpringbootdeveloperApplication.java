2150e1c7ff504294d3058ec75064a085264a144e
lgwk42 <luisgunkim@gmail.com>
2023-08-20 19:00:44 +0900

    스프링 공부 프로젝트

---
+++ src/main/java/com/study/springbootdeveloper/SpringbootdeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.study.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdeveloperApplication.class, args);
+	}
+
+}
